3498c89750c5a73fd274be61e054c36212074f36
Daniil <daniil@pas>
2021-02-20 14:26:15 +0300

    fix visibility after loading

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,10 @@
 const videoStart = 15;
 const videoEnd = 150;
 const videoPlaybackRate = 0.8;
+let loaded = false;
+let timerId = null;
+let opacity = 0;
+const maxOpacity = 0.5;
 
 window.onload = function() {
     let video = getVideo();     
@@ -21,8 +25,8 @@ function addVideoEvents(video) {
     video.addEventListener('loadeddata', function () {
         startVideo(video);
         videoLoaded(video);
-    });
-
+    });   
+   
     video.addEventListener('timeupdate', function () {
         if (video.currentTime >= videoEnd) {
             startVideo(video);
@@ -36,6 +40,18 @@ function startVideo(video) {
 }
 
 function videoLoaded(video) {
-    video.classList.add("loaded")
+    loaded = true;
+    goVisible(video);
+}
+
+function goVisible(video) {
+    opacity = opacity + 0.02;
+    video.style="opacity: " + opacity;
+
+    if (opacity >= maxOpacity){
+        return;
+    }
+
+    timerId = setTimeout(() => {goVisible(video)}, 33)
 }
 
