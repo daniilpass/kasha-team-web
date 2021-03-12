8ae2a9ddace327354ce010371df8a2d50a5ec391
Daniil <daniil@pas>
2021-03-12 14:21:22 +0300

    LOGO TRANSFORM

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
+
+
+window.onload = function() {
+    //video
+    let video = getVideo();     
+    addVideoEvents(video); 
+    setUpAndLoadVideo(video);
+
+    //logo
+    addLogoEvents();
+}
+
+/*
+* BACKGROUND VIDEO LOGIC
+*/
 const videoStart = 15;
 const videoEnd = 150;
 const videoPlaybackRate = 0.8;
@@ -5,12 +20,6 @@ let loaded = false;
 let timerId = null;
 let opacity = 0;
 const maxOpacity = 0.5;
-
-window.onload = function() {
-    let video = getVideo();     
-    addVideoEvents(video); 
-    setUpAndLoadVideo(video);
-}
 
 function getVideo() {
     return document.getElementById("video-cs");
@@ -55,3 +64,41 @@ function goVisible(video) {
     timerId = setTimeout(() => {goVisible(video)}, 33)
 }
 
+
+/*
+* LOGO TRANSFORM LOGIC
+*/
+const maxAngle = 20;
+
+function addLogoEvents() {
+    let wrapper = document.getElementById("logo-wrapper"); 
+    let img = document.getElementById("logo"); 
+
+    document.body.addEventListener('mousemove', e => {
+        bodyMouseMove(e, img, wrapper);
+    });
+}
+
+bodyMouseMove = function (e, img, wrapper) {
+    var rect = e.currentTarget.getBoundingClientRect();
+    offsetX = e.clientX - rect.left;
+    offsetY = e.clientY - rect.top;
+    offsetWidth =  wrapper.offsetWidth;
+    offsetHeight = window.innerHeight;
+
+    if (offsetX < wrapper.offsetLeft) {
+        offsetX = wrapper.offsetLeft
+    }
+    if (offsetX > wrapper.offsetLeft + wrapper.offsetWidth) {
+        offsetX = wrapper.offsetLeft + wrapper.offsetWidth
+    }
+    
+
+    let deltaX = ( (offsetX - wrapper.offsetLeft) / offsetWidth - 0.5) * 2 ;
+    let angleX = maxAngle * deltaX;
+
+    let deltaY = (offsetY / offsetHeight - 0.5) * 2 ;
+    let angleY = maxAngle * deltaY;
+
+    img.style.transform = `scale(1.2) perspective(500px) rotateY(${angleX}deg) rotateX(${-angleY}deg)`;
+}
